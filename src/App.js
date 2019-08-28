320b261f230ae55ab2f652ee9f208a7b212969c0
MFAC <ninafroes@gmail.com>
2019-08-28 12:36:39 +0200

    style: Turn app into flex-box container

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import Header from "./Components/Header";
 import Form from "./Components/Form";
 import Footer from "./Components/Footer";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer className="App">
       <Header />
       <Form />
       <Footer />
-    </div>
+    </AppContainer>
   );
 }
 
