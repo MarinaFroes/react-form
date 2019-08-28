8b2fdb7dde55214ded2ba4caf024385c9dd33d93
MFAC <ninafroes@gmail.com>
2019-08-28 12:37:13 +0200

    feat: Add phone number and submit and styles

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,18 +8,41 @@ const FormContainer = styled.form`
   width: 20rem;
   margin: 2rem;
   text-align: left;
-  border: 2px solid green;
+  padding: 1rem;
+  border: 2px solid #c0c0c0;
+`;
+
+const Label = styled.label`
+  margin: 0 1rem;
+`;
+
+const Input = styled.input`
+  margin: 1rem;
+  font-size: 1rem;
+`;
+
+const Submit = styled.input`
+  background-color: red;
+  color: #fff;
+  border-radius: 1rem;
+  padding: 0.2rem;
+  font-size: 1rem;
+  width: 10rem;
+  margin-left: 1rem;
 `;
 
 export default function Form() {
   return (
     <FormContainer id="form">
-      <label htmlFor="name">Name:</label>
-      <input type="text" name="name" id="name" />
-      <label htmlFor="email">E-mail:</label>
-      <input type="email" name="email" id="email" />
-      <label htmlFor="password">Password:</label>
-      <input type="password" name="password" id="password"/>
+      <Label htmlFor="name">Name:</Label>
+      <Input type="text" name="name" id="name" />
+      <Label htmlFor="number">Phone number:</Label>
+      <Input type="text" name="number" id="number" />
+      <Label htmlFor="email">E-mail:</Label>
+      <Input type="email" name="email" id="email" />
+      <Label htmlFor="password">Password:</Label>
+      <Input type="password" name="password" id="password" />
+      <Submit type="submit" value="Submit"/>
     </FormContainer>
   )
 };
