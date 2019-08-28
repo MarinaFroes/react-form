81392ce55d1f13e46795d05a7f33153bed6d0165
MFAC <ninafroes@gmail.com>
2019-08-28 11:52:23 +0200

    feat: Add input text, email and number

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,15 +3,23 @@ import styled from "styled-components";
 
 const FormContainer = styled.form`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
-  text-align: center;
+  width: 20rem;
+  margin: 2rem;
+  text-align: left;
   border: 2px solid green;
 `;
 
 export default function Form() {
   return (
     <FormContainer id="form">
+      <label htmlFor="name">Name:</label>
+      <input type="text" name="name" id="name" />
+      <label htmlFor="email">E-mail:</label>
+      <input type="email" name="email" id="email" />
+      <label htmlFor="password">Password:</label>
+      <input type="password" name="password" id="password"/>
     </FormContainer>
   )
 };
